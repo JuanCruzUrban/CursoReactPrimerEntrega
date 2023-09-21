3cf98d68773f761201720b15fb82712c158e80a6
Juan Cruz Urban <juanchi.urban@hotmail.com>
2023-09-21 20:39:30 -0300

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+import Header from './components/Header/Header';
+
+function App() {
+  
+  return (
+    
+      <div>
+        <Header/>
+      <h1>COMPONENTES</h1>
+      </div>
+      
+    
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="Header">
+
+<h1>Titulo</h1>
+
+    </div>
+  )
+}
+
+export default Header
