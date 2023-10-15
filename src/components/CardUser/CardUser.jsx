e6c65e49fe2099042b5acf80fa5b36e0d8609397
Juan Cruz Urban <juanchi.urban@hotmail.com>
2023-10-14 23:50:47 -0300

    segunda entrega Urban Juan Cruz

--- src/components/CardUser/CardUser.jsx
+++ src/components/CardUser/CardUser.jsx
@@ -4,31 +4,22 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-
-const CardUser = (el) =>  {
+import "./CardUser.css"
+const CardUser = ({el}) =>  {
 
   const {id, producto, precio, tipo} = el;
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="300"
-          image="https://i.pinimg.com/736x/5d/e8/d3/5de8d306cc6d0b27f7a74fdbbf1b93c8.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-           {props.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    
+    <div className='divCar'>
+      <h2>"id:" {id}</h2>
+      <h3>"Instrumento" {producto}</h3>
+     <p>"Precio $"{precio}</p> 
+      <p>"Tipo de Instrumento:" {tipo}</p>
+    </div>
+
+      
+
   );
 }
 
