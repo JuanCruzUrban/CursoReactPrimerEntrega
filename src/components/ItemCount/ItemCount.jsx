bbd42530eac522e35169457f75f0499abb4dbb8f
Juan Cruz Urban <juanchi.urban@hotmail.com>
2023-11-22 07:31:03 -0300

    seguimos en process

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,16 +3,23 @@ import "./ItemCount.css"
 import React from 'react'
 
 const ItemCount = () => {
-  const [cantidad, setCantidad] = useState(1)
+  const [cantidad, setCantidad] = useState(0)
+
+  const handleSumar = () => {
+    cantidad >= 0 && setCantidad(cantidad + 1)
+  }
   
-  
+  const handleRestar = () =>{
+        cantidad > 0 && setCantidad(cantidad - 1)  
+  }
+
     return (
         <div className="btnAgregarAlCarro">
            <div className="btnAgregarSacar">
            
-        <button className="buttonMas">+</button>
+        <button className="buttonMas" onClick={handleSumar}>+</button>
         <p className="cantidadNum">{cantidad}</p>
-        <button className="buttonMenos">-</button>
+        <button className="buttonMenos" onClick={handleRestar}>-</button>
         
            </div>
            <button className="agregar">Agregar Al Carrito</button>
